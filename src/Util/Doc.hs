6bd027a662f89d2099ec74d6eaebfbdc9ca49458
Ian-Woo Kim <ianwookim@gmail.com>
2017-04-18 23:00:29 +0000

    reimplement tagResultWithScore as tagText. now text marking is starting character (not one character before) to end character (1-based).

--- src/Util/Doc.hs
+++ src/Util/Doc.hs
@@ -4,9 +4,11 @@
 
 module Util.Doc where
 
-import           Control.Monad.Trans.State        (State,get,put)
+import           Control.Lens
+import           Control.Monad.Trans.State        (State,runState,get,put)
 import qualified Data.ByteString.Char8     as B   (drop,take)
-import           Data.List                        (intersperse)
+import           Data.Function                    (on)
+import           Data.List                        (intersperse,groupBy,sortBy)
 import           Data.Text                        (Text)
 import qualified Data.Text                 as T 
 import qualified Data.Text.Encoding        as TE  (decodeUtf8,encodeUtf8)
@@ -62,3 +64,23 @@ markOnly paraid Paragraph {..} = Paragraph (map (\(txt,mpid) -> (txt,convert mpi
   where
     convert Nothing = Nothing
     convert (Just pid) = if pid == paraid then Just pid else Nothing
+
+tagResult1 :: Text -> (tag,Int,Int) -> (Text,(tag,Text),Text)
+tagResult1 txt (s,b,e) =
+  let (t1,t1') = T.splitAt b txt
+      (t2,t2') = T.splitAt (e-b) t1'
+  in (t1,(s,t2),t2')
+
+-- | generalized from tagResultFromScore
+tagText :: [(tag,Int,Int)] -> Text -> [(Text,Maybe tag)]
+tagText []  rtxt = [(rtxt,Nothing)]
+tagText scs rtxt =
+  let idxs = sortBy (compare `on` (view _2)) scs
+      (tagged,(_,remaining)) =
+        flip runState (0,rtxt) . flip mapM idxs $ \(s,b,e) -> do
+          (n,t0) <- get
+          let (t1,(_,t2),t2') = tagResult1 t0 (s,b-n-1,e-n)
+          put (e,t2')
+          return ((t1,Nothing),(t2,Just s))
+      combine = Prelude.foldr (\(x,y) acc -> (x:).(y:).acc) id 
+  in (combine tagged . ((remaining,Nothing):)) [] 
--- test/test.hs
+++ test/test.hs
@@ -1,11 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Data.List (mapAccumL)
-import Data.Text (Text)
-import qualified Data.Text as T
+import           Data.Maybe      (isJust)
+import           Data.Text       (Text)
+import qualified Data.Text  as T
 --
-import View
-import Type
+import           Type
+import           View
+import           Util.Doc
+
 
 txt = "I just got reject by 500 Startups (company), Techsters and Y Combinator. And wrote e medium post about it. Here it goes. Lessons learned from a startup rejected by 500 Startups, Techstars and YCombinator The ups and downs, excitement and frustration, of being part of the selection process of the world’s top startup acceleration programs — and being rejected by all of them. By reading this article’s title you probably think my business is just bad — and it’s fine I won’t judge you — but please read the full story before reaching a conclusion."
 
@@ -14,25 +16,18 @@ ann0 = AnnotText [(T.take 30 txt,False)
                 ,((T.drop 30 . T.take 35) txt, True)
                 ,(T.drop 35 txt, False)
                 ]
-
+{-
 ann1 = AnnotText [(T.take 30 txt,False)
                 ,((T.drop 30 . T.take 35) txt, True)
                 ,((T.drop 35 . T.take 80) txt, False)
                 ]
 
 ann2 = AnnotText [((T.drop 80 . T.take 160) txt, False)]
-
-
-main = mapM_ cutePrintAnnot (lineSplitAnnot 80 ann0)
-{-
-main = do -- mapM_ cutePrintAnnot (lineSplitAnnot 80 ann)
-   --  cutePrintAnnot ann
-    -- cutePrintAnnot ann2
-    -- print (lineSplitAnnot 80 ann0)
-   let marked = markPosition (unAnnotText ann0)
-   print $ chunkAt 80 marked
-   let chunked = (chunkEveryAt 80 marked)
-   mapM_ print chunked
-   putStrLn "-------------------"
-   mapM_ cutePrintAnnot $ map (AnnotText . map (\(_,_,x)->x)) chunked 
 -}
+
+
+
+main = do
+  let ann = (AnnotText . map (\(t,m)->(t,isJust m)) . tagText [((),1,5),((),120,126)]) txt
+  -- print ann
+  mapM_ cutePrintAnnot (lineSplitAnnot 80 ann)
