b43e21ee484c00698653ade973ccaf570c2422bc
Ian-Woo Kim <ianwookim@gmail.com>
2017-04-19 15:04:52 +0000

    implement chunkLines

--- test/test.hs
+++ test/test.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+import           Data.List       (mapAccumL)
 import           Data.Maybe      (isJust)
 import           Data.Text       (Text)
 import qualified Data.Text  as T
@@ -7,27 +8,25 @@ import qualified Data.Text  as T
 import           Type
 import           View
 import           Util.Doc
+--
+import Debug.Trace
 
 
-txt = "I just got reject by 500 Startups (company), Techsters and Y Combinator. And wrote e medium post about it. Here it goes. Lessons learned from a startup rejected by 500 Startups, Techstars and YCombinator The ups and downs, excitement and frustration, of being part of the selection process of the world’s top startup acceleration programs — and being rejected by all of them. By reading this article’s title you probably think my business is just bad — and it’s fine I won’t judge you — but please read the full story before reaching a conclusion."
+txt = "I just got reject by 500 Startups (company), Techsters and Y Combinator. And wrote e medium post about it. Here it goes. Lessons learned from a startup rejected by 500 Startups, Techstars and YCombinator The ups and downs, excitement and frustration, of being part of the selection process of the world’s top startup acceleration programs — and being rejected by all of them.\n By reading this article’s title you probably think my business is just bad — and it’s fine I won’t judge you — but please read the full story before reaching a conclusion."
 
-
-ann0 = AnnotText [(T.take 30 txt,False)
-                ,((T.drop 30 . T.take 35) txt, True)
-                ,(T.drop 35 txt, False)
-                ]
-{-
-ann1 = AnnotText [(T.take 30 txt,False)
-                ,((T.drop 30 . T.take 35) txt, True)
-                ,((T.drop 35 . T.take 80) txt, False)
-                ]
-
-ann2 = AnnotText [((T.drop 80 . T.take 160) txt, False)]
--}
-
-
+txt2 = "a\nb\nc\nd"
+-- chunkLines :: (Int,Int,(Text,Bool)] -> [(Int,Int,(Text,Bool))]
+chunkLines (b,e,(t,m)) = let f (a,_) x = let l = T.length x in (a+l+1, x)
+                             ys = scanl f (0,"") (T.lines t)
+                             g ((a0,t0),(a1,t1)) = (b+a0,b+a1-2,t1)
+                         in map g (zip ys (tail ys))
 
 main = do
-  let ann = (AnnotText . map (\(t,m)->(t,isJust m)) . tagText [((),1,5),((),120,126)]) txt
+  let -- tagged = [(txt2,False)]
+  let tagged = (map (\(t,m)->(t,isJust m)) . tagText [((),15,20),((),120,126)]) txt
+  let ann = AnnotText tagged
+  mapM_ print (markPosition tagged)
+  putStrLn "================="
+  (mapM_ print . concatMap chunkLines . markPosition) tagged
   -- print ann
-  mapM_ cutePrintAnnot (lineSplitAnnot 80 ann)
+  -- mapM_ cutePrintAnnot (lineSplitAnnot 80 ann)
