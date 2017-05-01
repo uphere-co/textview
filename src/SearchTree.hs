c10b3eb8768a8a1538784c3c31c1622b8f0ab985
Ian-Woo Kim <ianwookim@gmail.com>
2017-05-01 22:23:37 +0000

    move parsers from etl library. adjust them with the change Forest Char -> Forest (Maybe Char)

--- src/SearchTree.hs
+++ src/SearchTree.hs
@@ -1,7 +1,12 @@
 module SearchTree where
 
+import           Control.Applicative
+import           Control.Monad                        (mzero,void)
+import           Data.Attoparsec.Text                 
+import qualified Data.Attoparsec.Internal.Types as AT (Parser(..),fromPos)
 import           Data.Function          (on)
 import           Data.List              (sortBy)
+import           Data.Maybe             (isNothing, catMaybes) 
 import           Data.Tree
 
 addTreeItem :: String -> Forest (Maybe Char) -> Forest (Maybe Char)
@@ -26,3 +31,38 @@ searchForest (x:xs) ts =
   in case rest' of
        []           -> [] 
        matched:rest -> searchForest xs (subForest matched)
+
+
+getPos :: Parser Int
+getPos = AT.Parser $ \t pos more _ succ' -> succ' t pos more (AT.fromPos pos)
+
+skipTill :: Alternative f => f a -> f b -> f b
+skipTill p end = scan'
+  where scan' = end <|> (p *> scan')
+
+tokencloser :: Parser ()
+tokencloser = void (satisfy (`elem` (" .,!?:;()-+=\"'`/\\|\8217\8220\8221" :: String))) <|> endOfInput
+
+pTree :: Forest (Maybe Char) -> [Char] -> Parser (String,Int)
+pTree forest acc = 
+  let lst = searchForest acc forest
+      lst' = catMaybes lst
+      term = filter isNothing lst
+  in (satisfy (\c -> c `elem` lst') >>= \x -> pTree forest (acc++[x]))
+     <|>
+     if Nothing `elem` lst then getPos >>= \e -> tokencloser >> return (acc,e) else mzero
+{-       
+  case lst of
+    [] -> do
+      e <- getPos 
+      tokencloser
+      return (acc,e)
+    _ -> do
+-}
+
+pTreeAdv :: Forest (Maybe Char) -> Parser (Int,Int,String)
+pTreeAdv forest = skipTill anyChar p
+  where p = do
+          b <- getPos
+          (x,e) <- pTree forest []
+          return (b+1,e,x)
--- test/search.hs
+++ test/search.hs
@@ -2,11 +2,13 @@
 
 module Main where
 
+import           Control.Applicative          (many)
 import           Control.Monad.IO.Class
 import           Control.Monad.Loops
-import           Data.Text           (Text)
-import qualified Data.Text    as T
-import qualified Data.Text.IO as TIO
+import           Data.Attoparsec.Text         (parseOnly)
+import           Data.Text                    (Text)
+import qualified Data.Text              as T
+import qualified Data.Text.IO           as TIO
 import           Data.Tree
 import           System.Console.Haskeline
 --
@@ -18,8 +20,8 @@ searchFunc ts str = fmap (\x->str++ f x) $ searchForest str ts
         f Nothing = "(END)"
 
   
-main :: IO ()
-main = do
+main' :: IO ()
+main' = do
   putStrLn "search"  
   txt <- TIO.readFile "F7745.all_entities"
   let lst = map ((\(a,b) -> (a,T.drop 1 b)) . T.breakOn "\t") . T.lines $ txt
@@ -29,3 +31,16 @@ main = do
   let forest = foldr addTreeItem [] nentities
   runInputT defaultSettings $ whileJust_ (getInputLine "% ") $ \input -> liftIO $ do
     print $ searchFunc forest input
+
+main' :: IO ()
+main' = do
+  putStrLn "search"  
+  txt <- TIO.readFile "F7745.all_entities"
+  let lst = map ((\(a,b) -> (a,T.drop 1 b)) . T.breakOn "\t") . T.lines $ txt
+      nentities = map (T.unpack . snd) lst
+  -- mapM_ putStrLn nentities
+
+  let forest = foldr addTreeItem [] nentities
+  let testtxt = "I think Intel will be the most successful in history."
+  
+  print (parseOnly (many (pTreeAdv forest)) testtxt)
