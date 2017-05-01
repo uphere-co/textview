65d6dbf181d05d0f011cb2a57eea7e9f1a76431c
Ian-Woo Kim <ianwookim@gmail.com>
2017-05-01 22:31:34 +0000

    add missing attoparsec in default.nix

--- default.nix
+++ default.nix
@@ -1,12 +1,12 @@
 { mkDerivation, base, bytestring, lens, split, stdenv, text
-, transformers
+, transformers, attoparsec
 }:
 mkDerivation {
   pname = "textview";
   version = "0.1.0.0";
   src = ./.;
   libraryHaskellDepends = [
-    base bytestring lens split text transformers
+    base bytestring lens split text transformers attoparsec
   ];
   license = "unknown";
 }
