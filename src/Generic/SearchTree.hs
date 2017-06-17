216970f250f9518dcda4e6aaa27ab341377b7bbf
Ian-Woo Kim <ianwookim@gmail.com>
2017-06-17 18:22:39 +0000

    remove unnecessary Ord and Show instances for search functions

--- src/Generic/SearchTree.hs
+++ src/Generic/SearchTree.hs
@@ -22,7 +22,7 @@ mkTree (x,(y:ys)) = Node (Just x) [mkTree (y,ys)]
 mkTree (x,[])     = Node (Just x) [Node Nothing []]
 
 
-searchForest :: (Eq a, Ord a) => [a] -> Forest (Maybe a) -> [Maybe a]
+searchForest :: (Eq a) => [a] -> Forest (Maybe a) -> [Maybe a]
 searchForest []     ts = map rootLabel ts
 searchForest (x:xs) ts =
   let (_prev,rest') = break (\t -> rootLabel t == Just x) ts
@@ -31,7 +31,7 @@ searchForest (x:xs) ts =
        matched:_    -> searchForest xs (subForest matched)
 
 
-searchFunc :: (Eq a, Ord a) => Forest (Maybe a) -> [a] -> [[a]]
+searchFunc :: (Eq a) => Forest (Maybe a) -> [a] -> [[a]]
 searchFunc ts str = fmap (\x->str++ f x) $ searchForest str ts
   where f (Just x) = [x]
         f Nothing = []
--- src/ParserCustom.hs
+++ src/ParserCustom.hs
@@ -11,9 +11,11 @@ import           Data.Tree
 --
 import           Generic.SearchTree
 
+
 type ParserG tok = EitherT String (State [tok])
 
-instance {-# OVERLAPPING #-}Alternative (ParserG tok) where
+
+instance {-# OVERLAPPING #-} Alternative (ParserG tok) where
   empty = EitherT (return (Left "error"))
   e1 <|> e2 = EitherT $ do
     s <- get
@@ -25,7 +27,7 @@ instance {-# OVERLAPPING #-}Alternative (ParserG tok) where
       Right r' -> return $ Right r' 
 
 
-pTreeG :: (Eq a, Ord a) => Forest (Maybe a) -> [a]-> ParserG a [a]
+pTreeG :: (Eq a) => Forest (Maybe a) -> [a]-> ParserG a [a]
 pTreeG forest acc = 
   let lst = searchForest acc forest
       lst' = catMaybes lst
@@ -34,14 +36,14 @@ pTreeG forest acc =
      if Nothing `elem` lst then return acc else left "not matched!"
 
 
-pTreeAdvG :: (Eq a, Ord a, Show a) => Forest (Maybe a) -> ParserG a [a]
+pTreeAdvG :: (Eq a) => Forest (Maybe a) -> ParserG a [a]
 pTreeAdvG forest = skipTillG anyTokenG p
   where p = do
           x <- pTreeG forest []
           return x
 
 
-anyTokenG :: (Show a) => ParserG a a
+anyTokenG :: ParserG a a
 anyTokenG = satisfyG $ const True
 
 
