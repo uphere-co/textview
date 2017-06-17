84da8d3768d741848c06db17f840dd735cbd6fdf
Ian-Woo Kim <ianwookim@gmail.com>
2017-06-17 05:19:05 +0000

    remove warnings

--- src/Generic/SearchTree.hs
+++ src/Generic/SearchTree.hs
@@ -1,23 +1,10 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeSynonymInstances #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE FlexibleContexts #-}
-
 module Generic.SearchTree where
 
-import           Control.Applicative
-import           Control.Monad                        (mzero,void)
-import           Data.Attoparsec.Text                 
-import qualified Data.Attoparsec.Internal.Types as AT (fromPos)
 import           Data.Function          (on)
 import           Data.List              (sortBy)
-import           Data.Maybe             (isNothing, catMaybes) 
-import           Data.Text              (Text)
-import qualified Data.Text              as T
-import qualified Data.Text.IO           as TIO
 import           Data.Tree
 --
+
 
 addTreeItem :: (Eq a, Ord a) => [a] -> Forest (Maybe a) -> Forest (Maybe a)
 addTreeItem []     ts = ts
@@ -28,18 +15,21 @@ addTreeItem (x:xs) ts =
        matched:rest ->
          let matched' = matched { subForest = addTreeItem xs (subForest matched)}
          in prev ++ (matched' : rest)
-          
+
+            
 mkTree :: (a,[a]) -> Tree (Maybe a)
 mkTree (x,(y:ys)) = Node (Just x) [mkTree (y,ys)]
 mkTree (x,[])     = Node (Just x) [Node Nothing []]
 
+
 searchForest :: (Eq a, Ord a) => [a] -> Forest (Maybe a) -> [Maybe a]
 searchForest []     ts = map rootLabel ts
 searchForest (x:xs) ts =
-  let (prev,rest') = break (\t -> rootLabel t == Just x) ts
+  let (_prev,rest') = break (\t -> rootLabel t == Just x) ts
   in case rest' of
        []           -> [] 
-       matched:rest -> searchForest xs (subForest matched)
+       matched:_    -> searchForest xs (subForest matched)
+
 
 searchFunc :: (Eq a, Ord a) => Forest (Maybe a) -> [a] -> [[a]]
 searchFunc ts str = fmap (\x->str++ f x) $ searchForest str ts
--- src/View.hs
+++ src/View.hs
@@ -12,35 +12,6 @@ import qualified Data.Text.IO           as TIO
 --
 import           Type
 
-checkLength :: AnnotText -> Int
-checkLength at = sum $ map (\x -> T.length (fst x)) (unAnnotText at)
-
-checkLengthUnA :: [(Text,Bool)] -> Int
-checkLengthUnA uat = sum $ map (\x -> T.length (fst x)) uat
-
-
-
-markPosition :: [(Text, Bool)] -> [(Int, Int, (Text, Bool))]
-markPosition xs = let xs' = scanl (\(a,_) y -> (a + T.length (fst y) ,y)) (0,("",False)) xs   
-                  in zipWith (\x0 x1 -> (fst x0+1,fst x1,snd x1)) xs' (tail xs')
-
-chunkAt :: Int
-        -> [(Int, Int, (Text, t))]
-        -> ([(Int, Int, (Text, t))], [(Int, Int, (Text, t))])
-chunkAt n lst = let (bef,aft) = break (\(b,e,_) -> n <= e) lst
-                in case aft of
-                     [] -> (bef,[])
-                     ((b,e,(t,m)):xs) -> let (t0,t1) = T.splitAt (n-b+1) t
-                                         in (bef ++[(b,n,(t0,m))],(n+1,e,(t1,m)):xs)
-
-chunkEveryAt :: Int -> [(Int, Int, (Text, t))]
-             -> [[(Int, Int, (Text, t))]]
-chunkEveryAt n [] = [] -- go 0 [] lst
-chunkEveryAt n lst@(y:ys) =  go (n0-1) [] lst
-  where (n0,_,_) = y
-        go m acc xs = let (bef,aft) = chunkAt (m+n) xs
-                      in if null aft then acc++[bef] else go (m+n) (acc++[bef]) aft
-
 
 data Chunk a = Chunked  a
              | Splitted
@@ -48,17 +19,47 @@ data Chunk a = Chunked  a
 
 unChunk :: Chunk t -> t
 unChunk (Chunked x) = x
+unChunk Splitted    = error "unChunk: Splitted"
+
+checkLength :: AnnotText -> Int
+checkLength = sum . map (\x -> T.length (fst x)) . unAnnotText
+
+
+checkLengthUnA :: [(Text,Bool)] -> Int
+checkLengthUnA = sum . map (\x -> T.length (fst x))
+
+
+markPosition :: [(Text, Bool)] -> [(Int, Int, (Text, Bool))]
+markPosition xs = let xs' = scanl (\(a,_) y -> (a + T.length (fst y) ,y)) (0,("",False)) xs   
+                  in zipWith (\x0 x1 -> (fst x0+1,fst x1,snd x1)) xs' (tail xs')
+
+
+chunkAt :: Int
+        -> [(Int, Int, (Text, t))]
+        -> ([(Int, Int, (Text, t))], [(Int, Int, (Text, t))])
+chunkAt n lst = let (bef,aft) = break (\(_b,e,_) -> n <= e) lst
+                in case aft of
+                     [] -> (bef,[])
+                     ((b,e,(t,m)):xs) -> let (t0,t1) = T.splitAt (n-b+1) t
+                                         in (bef ++[(b,n,(t0,m))],(n+1,e,(t1,m)):xs)
+
+
+chunkEveryAt :: Int -> [(Int, Int, (Text, t))]
+             -> [[(Int, Int, (Text, t))]]
+chunkEveryAt _n [] = []
+chunkEveryAt n lst@(y:_) =  go (n0-1) [] lst
+  where (n0,_,_) = y
+        go m acc xs = let (bef,aft) = chunkAt (m+n) xs
+                      in if null aft then acc++[bef] else go (m+n) (acc++[bef]) aft
+
 
 chunkLines :: (Int, t, (Text, t1)) -> [(Int, Int, Chunk (Text, t1))]
-chunkLines (b,e,(t,m)) = let f (a,_) (Chunked x) = let l = T.length x in (a+l, Chunked x)
-                             f (a,_) Splitted    = (a+1,Splitted)
-                             ys = scanl f (0,Chunked "") ((intersperse Splitted . map Chunked . T.lines) t)
-                             g ((a0,t0),(a1,t1)) = (b+a0,b+a1-1,fmap (\z->(z,m)) t1)
-                         in map g (zip ys (tail ys))
+chunkLines (b,_e,(t,m)) = let f (a,_) (Chunked x) = let l = T.length x in (a+l, Chunked x)
+                              f (a,_) Splitted    = (a+1,Splitted)
+                              ys = scanl f (0,Chunked "") ((intersperse Splitted . map Chunked . T.lines) t)
+                              g ((a0,_t0),(a1,t1)) = (b+a0,b+a1-1,fmap (\z->(z,m)) t1)
+                          in map g (zip ys (tail ys))
 
-
--- lineSplitAnnot :: Int -> AnnotText -> [AnnotText]
--- lineSplitAnnot n = map (AnnotText . map (\(_,_,x)->x)) . chunkEveryAt n . markPosition . unAnnotText
 
 lineSplitAnnot :: Int -> AnnotText -> [[AnnotText]]
 lineSplitAnnot n (AnnotText tagged) = 
@@ -66,10 +67,6 @@ lineSplitAnnot n (AnnotText tagged) =
       chunked = concatMap chunkLines marked
       renormed = map (map (\(b,e,x) -> (b,e,unChunk x))) . splitWhen (\(_,_,x) -> x == Splitted) $ chunked
   in map (map (AnnotText . map (\(_,_,x)->x)) . chunkEveryAt n) renormed
-  -- mapM_ print renormed
-
-
-        
 
 
 cutePrintAnnot :: AnnotText -> IO ()
