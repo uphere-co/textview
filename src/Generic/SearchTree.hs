2127fd5a60b762409c0faef054bd9c25f9d1b4fc
Ian-Woo Kim <ianwookim@gmail.com>
2017-06-17 18:31:23 +0000

    generalize searchForest to searchForestBy (with arbitrary equality check)

--- src/Generic/SearchTree.hs
+++ src/Generic/SearchTree.hs
@@ -22,18 +22,41 @@ mkTree (x,(y:ys)) = Node (Just x) [mkTree (y,ys)]
 mkTree (x,[])     = Node (Just x) [Node Nothing []]
 
 
+searchForestBy :: (a -> a -> Bool) -> [a] -> Forest (Maybe a) -> [Maybe a]
+searchForestBy _  []     ts = map rootLabel ts
+searchForestBy eq (x:xs) ts =
+  let Just a `meq` Just b = a `eq` b
+      Nothing `meq` Nothing = True
+      _ `meq` _ = False
+      (_prev,rest') = break (\t -> rootLabel t `meq` Just x) ts
+  in case rest' of
+       []           -> [] 
+       matched:_    -> searchForestBy eq xs (subForest matched)
+
+
 searchForest :: (Eq a) => [a] -> Forest (Maybe a) -> [Maybe a]
+searchForest = searchForestBy (==)
+{- 
 searchForest []     ts = map rootLabel ts
 searchForest (x:xs) ts =
   let (_prev,rest') = break (\t -> rootLabel t == Just x) ts
   in case rest' of
        []           -> [] 
        matched:_    -> searchForest xs (subForest matched)
+-}
 
 
-searchFunc :: (Eq a) => Forest (Maybe a) -> [a] -> [[a]]
-searchFunc ts str = fmap (\x->str++ f x) $ searchForest str ts
+searchFuncBy :: (a -> a -> Bool) -> Forest (Maybe a) -> [a] -> [[a]]
+searchFuncBy eq ts str = fmap (\x->str++ f x) $ searchForestBy eq str ts
   where f (Just x) = [x]
         f Nothing = []
 
+
+searchFunc :: (Eq a) => Forest (Maybe a) -> [a] -> [[a]]
+searchFunc = searchFuncBy (==)
+{- 
+searchFunc ts str = fmap (\x->str++ f x) $ searchForest str ts
+  where f (Just x) = [x]
+        f Nothing = []
+-}
 
