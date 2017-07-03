a1666f9bb594dea9ca42cfe2ce8df0a1174ea2a6
Ian-Woo Kim <ianwookim@gmail.com>
2017-07-03 20:01:02 +0000

    remove dead code

--- src/Text/Format/Tree.hs
+++ src/Text/Format/Tree.hs
@@ -28,29 +28,3 @@ linePrint' b (l,ls) s n
     drawlines False (l,ls) = T.concat (map (\x -> if x `elem` ls then "\x2502 " else "  ") [0..l-2]) <> "\x251C\x2500"
     drawlines True  (l,ls) = T.concat (map (\x -> if x `elem` ls then "\x2502 " else "  ") [0..l-2]) <> "\x2514\x2500"
 
- --       <> T.replicate (n-2) "  " 
-    -- drawlines xs = foldMap (\case True -> "\x2502 " ; False -> "  ") (init xs)
-    --                <> (\case True -> "\x251C\x2500" ; False -> "  ") (last xs)
-      
-
---                                  drawlines bs  <> btreePrint (bs++[False]) s b
-
-      -- "\x251C\x2500" <>
---  <> drawlines bs <> "\x2514\x2500")
-
-{- 
-bntPrint :: [Bool] -> (e -> Text) -> (a -> Text) ->  BNTree e a -> Text
-bntPrint _  _     lshow (BNTLeaf l)     = lshow l
-bntPrint bs nshow lshow (BNTNode e a b) =
-    "\x2299" <> nshow e <> "\n" <>
-    drawlines bs <> "\x251C\x2500" <> bntPrint (bs++[True]) nshow lshow a <> "\n" <>
-    drawlines bs <> "\x2514\x2500" <> bntPrint (bs++[False]) nshow lshow b
-  where
-    drawlines xs = foldMap (\case True -> "\x2502 " ; False -> "  ") xs
-
--- utility functions
-
-convert :: BinTree Text -> BNTree Text Text
-convert (BinLeaf a) = BNTLeaf a
-convert (BinNode a b) = BNTNode "node" (convert a) (convert b)
--}
